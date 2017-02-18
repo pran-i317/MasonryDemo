d8449ecb8745493415111d2c54ca609740584df5
Pranay Kumar Sanam <pranay.sanam@above-inc.com>
2017-02-18 17:43:56 +0530

    added append items demo

--- js/app.js
+++ js/app.js
@@ -8,3 +8,12 @@ for (let i=0;i<50;i++){
 }
 
 const masonry = new Masonry(masonryContainer, items);
+
+let extraItems = [];
+for (let i=0;i<25;i++){
+  const item = $('<div class="card" style="height:'+Math.floor((Math.random() * 200) + 125)+'px"><span class="card-content">'+(i+1)+'</span></div>');
+  extraItems.push(item);
+}
+setTimeout(() => {
+  masonry.appendItems(extraItems);
+}, 5000)
